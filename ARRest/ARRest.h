1695fa9d218a2ed8ed89c18b7cf078a0e8115cd8
Aryan Sharifian <Arish@Aryans-MacBook-Pro.local>
2014-06-05 16:00:06 -0700

    Initial Commit

---
+++ ARRest/ARRest.h
@@ -0,0 +1,19 @@
+//
+//  ARRest.h
+//  ARRest
+//
+//  Created by Aryan Sharifian on 6/5/14.
+//  Copyright (c) 2014 Aryan Sharifian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARRest.
+FOUNDATION_EXPORT double ARRestVersionNumber;
+
+//! Project version string for ARRest.
+FOUNDATION_EXPORT const unsigned char ARRestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARRest/PublicHeader.h>
+
+
